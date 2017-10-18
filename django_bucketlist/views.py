a876ec82f8550337e917d06d2ddb9373d8ac3287
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 19:02:16 +0300

    [Feature #152069040] wrapping API views
        - Ensure Request instances in the view are received
        - Provide behaviour such as returning 405 Method Not Allowed
        - Because the API chooses the content type of the response based on the client request, it will, by default, return an HTML-formatted representation of the resource when that resource is requested by a web browser. This allows for the API to return a fully web-browsable HTML representation.

--- django_bucketlist/views.py
+++ django_bucketlist/views.py
@@ -1,54 +1,51 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django_bucketlist.models import Bucketlist
 from django_bucketlist.serializers import BucketlistSerializer
 
 #  Because we want to be able to POST to this view from clients
 #  that won't have a CSRF token we need to mark the view as csrf_exempt
-@csrf_exempt
-def bucketlist_list(request):
+@api_view(['GET', 'POST'])
+def bucketlist_list(request, format=None):
     """
     List all bucket lists or create a bucketlist.
     """
     if request.method == 'GET':
         bucketlists = Bucketlist.objects.all()
         serializer = BucketlistSerializer(bucketlists, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = BucketlistSerializer(data=data)
+        serializer = BucketlistSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # view which corresponds to an individual bucketlist
 
-@csrf_exempt
-def bucketlist_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def bucketlist_detail(request, pk, format=None):
     """
     Retrieve, update or delete a bucketlist.
     """
     try:
         bucketlist = Bucketlist.objects.get(pk=pk)
     except Bucketlist.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = BucketlistSerializer(bucketlist)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = BucketlistSerializer(bucketlist, data=data)
+        serializer = BucketlistSerializer(bucketlist, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         bucketlist.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
