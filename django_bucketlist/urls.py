fd15761ff33bbb69b6ae49035f26e2050485641d
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 19:34:16 +0300

    [Feature #152069040] Refactor in favor of class-based views

--- django_bucketlist/urls.py
+++ django_bucketlist/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from django_bucketlist import views
 
 urlpatterns = [
-    url(r'^bucketlists/$', views.bucketlist_list),
-    url(r'^bucketlists/(?P<pk>[0-9]+)/$', views.bucketlist_detail),
+    url(r'^bucketlists/$', views.BucketlistList.as_view()),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/$', views.BucketlistDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- django_bucketlist/views.py
+++ django_bucketlist/views.py
@@ -1,22 +1,20 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from django_bucketlist.models import Bucketlist
 from django_bucketlist.serializers import BucketlistSerializer
+from django.http import Http404
 
-#  Because we want to be able to POST to this view from clients
-#  that won't have a CSRF token we need to mark the view as csrf_exempt
-@api_view(['GET', 'POST'])
-def bucketlist_list(request, format=None):
+class BucketlistList(APIView):
     """
     List all bucket lists or create a bucketlist.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         bucketlists = Bucketlist.objects.all()
         serializer = BucketlistSerializer(bucketlists, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = BucketlistSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -25,27 +23,30 @@ def bucketlist_list(request, format=None):
 
 # view which corresponds to an individual bucketlist
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def bucketlist_detail(request, pk, format=None):
+class BucketlistDetail(APIView):
     """
-    Retrieve, update or delete a bucketlist.
+    Retrieve, update or delete a bucketlist instance.
     """
-    try:
-        bucketlist = Bucketlist.objects.get(pk=pk)
-    except Bucketlist.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+           return Bucketlist.objects.get(pk=pk)
+        except Bucketlist.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format= None):
+        bucketlist = self.get_object(pk)
         serializer = BucketlistSerializer(bucketlist)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format= None):
+        bucketlist = self.get_object(pk)
         serializer = BucketlistSerializer(bucketlist, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format= None):
+        bucketlist = self.get_object(pk)
         bucketlist.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
