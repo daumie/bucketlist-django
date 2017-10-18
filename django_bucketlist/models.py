4a917a45beff2652bb9ebd99048d91e157b0c037
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 10:43:08 +0300

    [Fix] Correctly call the Model class

--- django_bucketlist/models.py
+++ django_bucketlist/models.py
@@ -1,16 +1,10 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
 
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
-
-class Bucketlist(models.model):
+class Bucketlist(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     description = models.TextField()
-    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    style = models.CharField(default='friendly', max_length=100)
 
     class Meta:
         ordering = ('created',)
