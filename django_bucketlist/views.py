5eb6ed9f586c7245a35353ea4bfac999db6b39ee
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 14:10:12 +0300

    [Feature #152069040] list all existing bucketlists
     - lists all available bucketlist at API root
     - creates a new bucketlist at API root

--- django_bucketlist/views.py
+++ django_bucketlist/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from django_bucketlist.models import Bucketlist
+from django_bucketlist.serializers import BucketlistSerializer
 
-# Create your views here.
+#  Because we want to be able to POST to this view from clients
+#  that won't have a CSRF token we need to mark the view as csrf_exempt
+@csrf_exempt
+def bucketlist_list(request):
+    """
+    List all bucket lists or create a bucketlist.
+    """
+    if request.method == 'GET':
+        bucketlist = Bucketlist.objects.all()
+        serializer = BucketlistSerializer(bucketlist, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = BucketlistSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+# view which corresponds to an individual bucketlist
+
