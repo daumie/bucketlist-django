6f33c5fc4c1f7bfc9dccea8e5c6947dce5660aa1
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 10:36:37 +0300

    [Feature #152062328] Add bucketlist as one of the installed apps

--- bucketlist/settings.py
+++ bucketlist/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_bucketlist.apps.Django_bucketlistConfig',
+    'django_bucketlist'
 ]
 
 MIDDLEWARE = [
