65981b23bc26c844693980262e48257f52e6fd84
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 23:43:26 +0300

    [Feature #151996741] Add login views
        - Adds userlist and userdetail views to login

--- django_bucketlist/views.py
+++ django_bucketlist/views.py
@@ -1,6 +1,9 @@
 from django_bucketlist.models import Bucketlist
 from django_bucketlist.serializers import BucketlistSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
+from django_bucketlist.serializers import UserSerializer
+from rest_framework import permissions
 
 class BucketlistList(generics.ListCreateAPIView):
     """
@@ -8,6 +11,10 @@ class BucketlistList(generics.ListCreateAPIView):
     """
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
     
 # view which corresponds to an individual bucketlist
 
@@ -18,3 +25,13 @@ class BucketlistDetail(generics.RetrieveUpdateDestroyAPIView):
   
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
