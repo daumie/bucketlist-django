b039aed11abf99325d094355eb88e9705866c793
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 23:41:08 +0300

    [Feature #151996741] Add user model
        - Adds user model and associates it with bucketlist

--- django_bucketlist/models.py
+++ django_bucketlist/models.py
@@ -11,7 +11,11 @@ class Bucketlist(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     description = models.TextField()
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='bucketlist', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('created',)
+
+    def save(self, *args, **kwargs):
+        super(Bucketlist, self).save(*args, **kwargs)
 
