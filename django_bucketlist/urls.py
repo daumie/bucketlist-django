8ecd14a797e4e3ae4aa2368de1e699e05c0b583d
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 19:01:33 +0300

    [Feature #152069040] Add format suffix patterns

--- django_bucketlist/urls.py
+++ django_bucketlist/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from django_bucketlist import views
 
 urlpatterns = [
     url(r'^bucketlists/$', views.bucketlist_list),
     url(r'^bucketlists/(?P<pk>[0-9]+)/$', views.bucketlist_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
