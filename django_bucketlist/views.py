2d06e93a6874a4116842cfec7d0aa070db11b590
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 19:53:57 +0300

    [Feature #152069040] Refactor using mixed-in generic views

--- django_bucketlist/views.py
+++ django_bucketlist/views.py
@@ -1,41 +1,20 @@
 from django_bucketlist.models import Bucketlist
 from django_bucketlist.serializers import BucketlistSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-class BucketlistList(mixins.ListModelMixin,
-                     mixins.CreateModelMixin,
-                     generics.GenericAPIView):
+class BucketlistList(generics.ListCreateAPIView):
     """
     List all bucket lists or create a bucketlist.
     """
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-
+    
 # view which corresponds to an individual bucketlist
 
-class BucketlistDetail(mixins.RetrieveModelMixin,
-                       mixins.UpdateModelMixin,
-                       mixins.DestroyModelMixin,
-                       generics.GenericAPIView):
+class BucketlistDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a bucketlist instance.
     """
   
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-      return self.destroy(request, *args, **kwargs)
