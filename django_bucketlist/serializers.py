c9b12248ddf36c4df9af8fd7010060307d673aa3
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 23:45:01 +0300

    [Feature #151996741] Add user serializers

--- django_bucketlist/serializers.py
+++ django_bucketlist/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 from django_bucketlist.models import Bucketlist, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 
 class BucketlistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bucketlist
-        fields =('id', 'title', 'description', 'style')
+        fields =('id', 'title', 'description', 'style', 'owner')
+        owner = serializers.ReadOnlyField(source='owner.username')
+
+class UserSerializer(serializers.ModelSerializer):
+    bucketlist = serializers.PrimaryKeyRelatedField(many=True, queryset=Bucketlist.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'bucketlist')
 
