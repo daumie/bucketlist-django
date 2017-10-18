5f4b30daf8e457e4239bf6ce28f2a7e8faf4126a
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 13:43:17 +0300

    [Feature #152062328] Add styling choice using pygments library

--- django_bucketlist/models.py
+++ django_bucketlist/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from pygments.styles import get_all_styles
+
+
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 class Bucketlist(models.Model):
+    """Model for creation of a bucketlist"""
+
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     description = models.TextField()
-    style = models.CharField(default='friendly', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
     class Meta:
         ordering = ('created',)
