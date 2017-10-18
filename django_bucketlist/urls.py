d70adf5c8dbc7e1c4cff9ff11f3199d02d396bcd
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-18 16:50:30 +0300

    [Feature #152069040] Wire up bbucketlists views to app urlconf

--- django_bucketlist/urls.py
+++ django_bucketlist/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from django_bucketlist import views
 
 urlpatterns = [
-    url(r'^bucketlist/$', views.bucketlist_list),
-    url(r'^bucketlist/(?P<pk>[0-9]+)/$', bucketlist_detail),
+    url(r'^bucketlists/$', views.bucketlist_list),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/$', views.bucketlist_detail),
 ]
