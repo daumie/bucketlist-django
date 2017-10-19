24c4c9ecaa01ace5da4b8a8b90a27d7f7c663bc4
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-19 18:41:20 +0300

    [Feaeture #151996741] Redirect bucketlists view

--- django_bucketlist/urls.py
+++ django_bucketlist/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 from rest_framework.urlpatterns import format_suffix_patterns
 from django_bucketlist import views
 
 urlpatterns = [
-    url(r'^bucketlists/$', views.BucketlistList.as_view()),
+    url(r'^bucketlists/$', views.BucketlistList.as_view(), name='bucketlist'),
+    url(r'^$', RedirectView.as_view(pattern_name='bucketlist')),
     url(r'^bucketlists/(?P<pk>[0-9]+)/$', views.BucketlistDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
