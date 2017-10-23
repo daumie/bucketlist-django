832ebd7733b996c34e5d62fbc0bdbc44b2f6b66c
Dominic Motuka <dominic.motuka@gmail.com>
2017-10-23 18:07:49 +0300

    [Feature  #152069040] Add model fiels for items

--- django_bucketlist/models.py
+++ django_bucketlist/models.py
@@ -8,11 +8,25 @@ class Bucketlist(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     description = models.TextField()
-    owner = models.ForeignKey('auth.User', related_name='bucketlist', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='+', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('created',)
 
     def save(self, *args, **kwargs):
         super(Bucketlist, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class Item(models.Model):
+    """Model for creation of an item"""
+
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField()
+    bucketlist = models.ForeignKey('Bucketlist', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        super(Item, self).save(*args, **kwargs)
 
